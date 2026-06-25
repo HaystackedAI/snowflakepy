16abb16a715415ad7373edc30f65d12e64649a69
AI Accounting <aiaccfin@gmail.com>
2026-06-24 20:06:30 -0400

    db/table

--- app/conn.py
+++ app/conn.py
@@ -30,7 +30,20 @@ try:
     print(f"Snowflake version: {one_row[0]}")
     cs.execute("CREATE WAREHOUSE IF NOT EXISTS tiny_warehouse_mg")
     cs.execute("USE WAREHOUSE tiny_warehouse_mg")
+    cs.execute("CREATE DATABASE IF NOT EXISTS testdb")
+    cs.execute("USE DATABASE testdb")
+    cs.execute("CREATE SCHEMA IF NOT EXISTS testschema")
+    cs.execute("USE SCHEMA testdb.testschema")
+    cs.execute(
+            "CREATE OR REPLACE TABLE "
+            "test_table(col1 integer, col2 string)")
+    cs.execute(
+        "INSERT INTO test_table(col1, col2) "
+        "VALUES(123, 'test string1'),(456, 'test string2')")
 
+    col1, col2 = cs.execute("SELECT col1, col2 FROM test_table").fetchone()
+    print('{0}, {1}'.format(col1, col2))
+    for (col1, col2) in cs.execute("SELECT col1, col2 FROM test_table"):print('{0}, {1}'.format(col1, col2))
 finally:
     cs.close()
 
